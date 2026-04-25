48c9af7b3c4b897c1e777300a9704e35a07fb147
Ming Hieu <hieunm1987@gmail.com>
2026-04-25 07:12:06 +0700

    deployment

--- frontend/src/hooks/useAPI.ts
+++ frontend/src/hooks/useAPI.ts
@@ -1,8 +1,6 @@
-import { useNavigate } from "react-router-dom"
-import wretch, { WretchOptions } from "wretch"
+import wretch from "wretch"
 import FormDataAddon from "wretch/addons/formData"
-import { LS_TOKEN_KEY } from "common/constant"
-import { getLanguage, getToken } from "common/helper"
+import { getLanguage } from "common/helper"
 import { LoginBodyType } from "pages/Login/types"
 
 const EP = { // end points
@@ -15,12 +13,12 @@ const EP = { // end points
 }
 
 export const useAPI = () => {
-	const navigate = useNavigate()
+	// const navigate = useNavigate()
 	const CLIENT_BASE_URL = `${import.meta.env.VITE_BACKEND_BASE_URL}/api`
-	const wretchOptions: WretchOptions = {
-		credentials: "include",
-		mode: "cors"
-	}
+	// const wretchOptions: WretchOptions = {
+	// 	credentials: "include",
+	// 	mode: "cors"
+	// }
 
 	// wretch request without permission send with cookie
 	const request = wretch(CLIENT_BASE_URL)
@@ -31,51 +29,51 @@ export const useAPI = () => {
 	const requestWithCookie = request
 		.options({ credentials: "include" })
 
-	const subscribers: ((token: string) => void)[] = []
+	// const subscribers: ((token: string) => void)[] = []
 
-	const notifySubscribers = (token: string) => {
-		subscribers.forEach(cb => cb(token))
-		// empty subscribers array
-		subscribers.length = 0
-	}
+	// const notifySubscribers = (token: string) => {
+	// 	subscribers.forEach(cb => cb(token))
+	// 	// empty subscribers array
+	// 	subscribers.length = 0
+	// }
 
-	const refreshAccessToken = async (currentToken: string) => {
-		const newAccessToken = await requestWithCookie
-			.auth(`Bearer ${currentToken}`)
-			.url(EP.refreshToken)
-			.options(wretchOptions)
-			.post() // refresh token should get from cookie from backend
-			.text()
+	// const refreshAccessToken = async (currentToken: string) => {
+	// 	const newAccessToken = await requestWithCookie
+	// 		.auth(`Bearer ${currentToken}`)
+	// 		.url(EP.refreshToken)
+	// 		.options(wretchOptions)
+	// 		.post() // refresh token should get from cookie from backend
+	// 		.text()
 
-		localStorage.setItem(LS_TOKEN_KEY, newAccessToken)
-		return newAccessToken
-	}
+	// 	localStorage.setItem(LS_TOKEN_KEY, newAccessToken)
+	// 	return newAccessToken
+	// }
 
-	const authFetch = (path: string) => {
-		const accessToken = getToken()
-		return requestWithCookie
-			.url(path)
-			.auth(`Bearer ${accessToken}`)
-			.resolve(r => r)
-			.catcher(401, async (_, request) => {
-				try {
-					// attach current access token to refresh token request's header
-					const newToken = await refreshAccessToken(accessToken)
-					notifySubscribers(newToken)
-					return request
-						.auth(`Bearer ${newToken}`)
-						.headers({ "skip-auth": "true" })
-						.fetch()
-						.json(r => r)
-				} catch (err) {
-					console.error("Token refresh failed", err)
-					await logout(accessToken)
-					localStorage.removeItem(LS_TOKEN_KEY)
-					navigate("/login")
-					throw err
-				}
-			})
-	}
+	// const authFetch = (path: string) => {
+	// 	const accessToken = getToken()
+	// 	return requestWithCookie
+	// 		.url(path)
+	// 		.auth(`Bearer ${accessToken}`)
+	// 		.resolve(r => r)
+	// 		.catcher(401, async (_, request) => {
+	// 			try {
+	// 				// attach current access token to refresh token request's header
+	// 				const newToken = await refreshAccessToken(accessToken)
+	// 				notifySubscribers(newToken)
+	// 				return request
+	// 					.auth(`Bearer ${newToken}`)
+	// 					.headers({ "skip-auth": "true" })
+	// 					.fetch()
+	// 					.json(r => r)
+	// 			} catch (err) {
+	// 				console.error("Token refresh failed", err)
+	// 				await logout(accessToken)
+	// 				localStorage.removeItem(LS_TOKEN_KEY)
+	// 				navigate("/login")
+	// 				throw err
+	// 			}
+	// 		})
+	// }
 
 	const login = (form: LoginBodyType) => requestWithCookie.url(EP.login)
 							.addon(FormDataAddon)
@@ -106,6 +104,7 @@ export const useAPI = () => {
 	}
 
 	return {
-		login
+		login,
+    logout
 	}
 }
